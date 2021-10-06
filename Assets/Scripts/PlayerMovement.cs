5b7430fc8286343058753b2d627bb0e97103a3ba
Franci <algera.franci@gmail.com>
2021-10-06 16:37:01 +0200

    wall collision

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,39 @@ using Rewired;
 public class PlayerMovement : MonoBehaviour
 {
     public int playerID = 0;
+    public SpriteRenderer sprite;
     private Player player;
+    [SerializeField] float deadZoneController;
+    [SerializeField] float bufferTime;
+    [SerializeField] float raycastDistance;
+    [SerializeField] Color playerColor;
     float mvtHorizontal;
     float jump;
     float inputTimer;
     float afterBeatTimer;
-    [SerializeField] float bufferTime;
     bool buttonPressed;
     bool beatPassed;
     bool hasMoved;
-    [SerializeField]  bool hasJumped;
-    public SpriteRenderer sprite;
-    [SerializeField] bool isOnFloor;
-    [SerializeField] bool wasInAir;
-    [SerializeField] float raycastDistance;
+    bool hasJumped;
+    bool isOnFloor;
+    bool wasInAir;
+    bool buttonDown;
+    bool canGoRight;
+    bool canGoLeft;
+    bool canjump;
 
     private void Start()
     {
         player = ReInput.players.GetPlayer(playerID);
         RhythmManager.Instance.onMusicBeatDelegate += BeatReceived;
+        sprite.color = playerColor;
     }
 
     private void Update()
     {
         GetInput();
         Gravity();
+        WallCollision();
         if (buttonPressed)
         {
             inputTimer += Time.deltaTime;
@@ -50,42 +58,48 @@ public class PlayerMovement : MonoBehaviour
 
     public void GetInput()
     {
-        //move horizontal
-        if(player.GetAxis("Move Horizontal") !=0 && !beatPassed)
+        //move horizontal 
+        bool inputHorizontal = player.GetAxis("Move Horizontal") < -deadZoneController || Input.GetAxis("Horizontal") > deadZoneController;
+
+        if (inputHorizontal && !beatPassed && !buttonDown)
         {
             buttonPressed = true;
+            buttonDown = true;
             mvtHorizontal = player.GetAxis("Move Horizontal");
 
         }
-        else if(player.GetAxis("Move Horizontal") != 0 && beatPassed && afterBeatTimer < bufferTime && !hasMoved)
+        else if (inputHorizontal && beatPassed && afterBeatTimer < bufferTime && !hasMoved && !buttonDown)
         {
+            buttonDown = true;
             mvtHorizontal = player.GetAxis("Move Horizontal");
             Move();
             afterBeatTimer = 0;
         }
-        else if(Input.GetAxis("Horizontal") > -.1f && Input.GetAxis("Horizontal") < .1f)
+        else if (buttonDown && player.GetAxis("Move Horizontal") > -deadZoneController && Input.GetAxis("Horizontal") < deadZoneController)
         {
             mvtHorizontal = 0;
+            buttonDown = false;
         }
 
         //jump
-        if(player.GetAxis("Jump") > 0 && !beatPassed)
+        if (player.GetButtonDown("Jump") && !beatPassed)
         {
             buttonPressed = true;
             jump = 1;
 
         }
-        else if (player.GetAxis("Jump") > 0 && beatPassed && afterBeatTimer < bufferTime && !hasMoved)
+        else if (player.GetButtonDown("Jump") && beatPassed && afterBeatTimer < bufferTime && !hasMoved)
         {
             jump = 1;
             mvtHorizontal = 0;
             Move();
             afterBeatTimer = 0;
         }
-        else if (player.GetAxis("Jump") < .1f)
+        else
         {
             jump = 0;
         }
+
 
     }
 
@@ -93,14 +107,20 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!isOnFloor && (mvtHorizontal == 0 || wasInAir || !hasJumped)) // if for gravity 
         {
-            Debug.Log("gravity");
             transform.DOMoveY(transform.position.y - 1, .2f);
             hasMoved = true;
             wasInAir = true;
         }
         else if (!isOnFloor && inputTimer < bufferTime && mvtHorizontal != 0 && !wasInAir) // move after jump
         {
-            int i = mvtHorizontal > 0 ? 1 : -1;
+            int i = 0;
+            if (mvtHorizontal > 0 && canGoRight)
+            {
+                i = 1;
+            }else if(mvtHorizontal < 0 && canGoLeft)
+            {
+                i = -1;
+            }
             transform.DOMoveX(transform.position.x + i, .2f);
             buttonPressed = false;
             hasMoved = true;
@@ -109,7 +129,15 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (isOnFloor && inputTimer < bufferTime && mvtHorizontal !=0) //move horizontal on floor
         {
-            int i = mvtHorizontal > 0 ? 1 : -1;
+            int i = 0;
+            if (mvtHorizontal > 0 && canGoRight)
+            {
+                i = 1;
+            }
+            else if (mvtHorizontal < 0 && canGoLeft)
+            {
+                i = -1;
+            }
             transform.DOMoveX(transform.position.x + i, .2f);
             buttonPressed = false;
             hasMoved = true;
@@ -117,7 +145,10 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (isOnFloor && inputTimer < bufferTime && jump > 0 )// jump
         {
-            transform.DOMoveY(transform.position.y + jump, .2f);
+            if (canjump)
+            {
+                transform.DOMoveY(transform.position.y + jump, .2f);
+            }
             jump = 0;
             buttonPressed = false;
             hasMoved = true;
@@ -142,9 +173,9 @@ public class PlayerMovement : MonoBehaviour
     //raycast O.O *u* hello there :)))) watcha ray casting on?
     public void Gravity()
     {
-        RaycastHit2D rayray = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, LayerMask.GetMask("Ground"));
+        RaycastHit2D rayrayFall = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, LayerMask.GetMask("Ground"));
 
-        if( rayray.collider != null)
+        if( rayrayFall.collider != null)
         {
             isOnFloor = true;
             if(wasInAir)
@@ -154,6 +185,37 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             isOnFloor = false;
+        }
+    }
+
+    public void WallCollision()
+    {
+        RaycastHit2D rayrayRight = Physics2D.Raycast(transform.position, Vector2.right, 1, LayerMask.GetMask("Ground"));
+        if (rayrayRight.collider != null)
+        {
+            canGoRight = false;
+        }
+        else
+        {
+            canGoRight = true;
+        }
+        RaycastHit2D rayrayLeft = Physics2D.Raycast(transform.position, Vector2.left, 1, LayerMask.GetMask("Ground"));
+        if (rayrayLeft.collider != null)
+        {
+            canGoLeft = false;
+        }
+        else
+        {
+            canGoLeft = true;
+        }
+        RaycastHit2D rayrayJump = Physics2D.Raycast(transform.position, Vector2.up, 1, LayerMask.GetMask("Ground"));
+        if (rayrayJump.collider != null)
+        {
+            canjump = false;
+        }
+        else
+        {
+            canjump = true;
         }
     }
 
@@ -171,27 +233,29 @@ public class PlayerMovement : MonoBehaviour
 
         if (inputTimer < bufferTime / 3 || afterBeatTimer < bufferTime / 3)
         {
-            Debug.Log("<color=green> Good </color>");
+            //Debug.Log("<color=green> Good </color>");
             seqColor.Append(sprite.DOColor(Color.green, .1f));
         }
         else if (inputTimer < bufferTime / 3 * 2 || afterBeatTimer < bufferTime / 3 * 2)
         {
-            Debug.Log("<color=yellow> Okay </color>");
+            //Debug.Log("<color=yellow> Okay </color>");
             seqColor.Append(sprite.DOColor(Color.yellow, .1f));
         }
         else if (inputTimer < bufferTime  || afterBeatTimer < bufferTime )
         {
-            Debug.Log("<color=orange> Early / Late</color>");
+            //Debug.Log("<color=orange> Early / Late</color>");
             seqColor.Append(sprite.DOColor(new Color(1, .5f ,0), .1f));
         }
         
-        seqColor.Append(sprite.DOColor(Color.white, .1f));
+        seqColor.Append(sprite.DOColor(playerColor, .1f));
     }
 
     #endregion
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - raycastDistance));
-    }
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x +1, transform.position.y));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x -1, transform.position.y));
+    }*/
 }
