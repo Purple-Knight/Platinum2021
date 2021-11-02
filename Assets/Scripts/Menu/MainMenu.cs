641dcf92127aad760e1ec7ff3b43c2a813dbbc42
LucaSchifano <luca.schifano005@gmail.com>
2021-11-02 16:22:46 +0100

    Menu option completed ( sliders + save et tt ) !

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -21,13 +21,18 @@ public class MainMenu : MonoBehaviour
     public MenuState state;
 
 
-
     //Menu Var -------------------------------------------
     int cursorPos;
+    int cursorPosOption;
     public GameObject Cursor;
     [SerializeField] List<Transform> cPosition = new List<Transform>();
+    [SerializeField] List<Transform> cPositionOption = new List<Transform>();
     List<bool> once = new List<bool> { false, false, false, false };
     public float deadZone;
+
+
+    // Slider --------------------------------------------
+    public List<Slider> Sliders = new List<Slider>();
 
     // ------------------------------------
     bool forChecking;
@@ -37,6 +42,7 @@ public class MainMenu : MonoBehaviour
         TITLE,
         MENU,
         CHARSELECT,
+        OPTION,
     }
 
     public void Awake()
@@ -45,6 +51,11 @@ public class MainMenu : MonoBehaviour
 
         checkController();
 
+    }
+
+    private void Start()
+    {
+        loadMusicVolume();
     }
 
     public void checkController()
@@ -56,6 +67,9 @@ public class MainMenu : MonoBehaviour
             if(!players.Contains(ReInput.players.GetPlayer(i))) players.Add(ReInput.players.GetPlayer(i));
         }
     }
+
+
+
 
     void Update()
     {
@@ -70,6 +84,9 @@ public class MainMenu : MonoBehaviour
                         toMenu();
                     }
                     break;
+
+
+
 
                 case MenuState.MENU:
 
@@ -104,6 +121,9 @@ public class MainMenu : MonoBehaviour
                                 forChecking = true;
                                 break;
                             case 2:
+                                toOption();
+                                break;
+                            case 3:
                                 toTitle();
                                 break;
                             default:
@@ -114,8 +134,63 @@ public class MainMenu : MonoBehaviour
                     break;
 
 
+
+
+
+
                 case MenuState.CHARSELECT:
                     break;
+
+
+
+
+                case MenuState.OPTION:
+
+                    if (once[(players.IndexOf(item))] == false && item.GetAxisRaw("MenuVertical") > 0 + deadZone)
+                    {
+                        if (cursorPosOption > 0) cursorPosOption--;
+                        once[(players.IndexOf(item))] = true;
+                        setCursor();
+                    }
+
+                    else if (once[(players.IndexOf(item))] == false && item.GetAxisRaw("MenuVertical") < 0 - deadZone)
+                    {
+                        if (cursorPosOption < cPositionOption.Count - 1) cursorPosOption++;
+                        once[(players.IndexOf(item))] = true;
+                        setCursor();
+
+                    }
+
+                    if (once[(players.IndexOf(item))] == false && item.GetAxisRaw("MenuHorizontal") > 0 + deadZone)
+                    {
+                        changeSliders(true);
+                        once[(players.IndexOf(item))] = true;
+                    }
+
+
+                    else if (once[(players.IndexOf(item))] == false && item.GetAxisRaw("MenuHorizontal") < 0 - deadZone)
+                    {
+                        changeSliders(false);
+                        once[(players.IndexOf(item))] = true;
+                    }
+
+
+                    else if (item.GetAxisRaw("MenuVertical") < deadZone && item.GetAxisRaw("MenuVertical") > -deadZone
+                        && item.GetAxisRaw("MenuHorizontal") < deadZone && item.GetAxisRaw("MenuHorizontal") > -deadZone)
+                    {
+                        once[(players.IndexOf(item))] = false;
+                        setCursor();
+                    }
+
+
+
+                    if (item.GetButtonDown("Confirm"))
+                    {
+                        toMenu();
+                    }
+
+                    break;
+
                 default:
                     break;
             }
@@ -130,38 +205,106 @@ public class MainMenu : MonoBehaviour
     }
 
 
+
+
+    void changeSliders(bool plus)
+    {
+        if(cursorPosOption < Sliders.Count)
+        {
+            if (plus) Sliders[cursorPosOption].value += 5;
+            else Sliders[cursorPosOption].value -= 5;
+
+            SetVolumeGen();
+            SetVolumeMusic();
+            SetVolumeSFX();
+        }
+    }
+
     void setCursor()
     {
-        Cursor.GetComponent<RectTransform>().transform.position = cPosition[cursorPos].position;
+        if(state == MenuState.MENU) Cursor.GetComponent<RectTransform>().transform.position = cPosition[cursorPos].position;
+        if(state == MenuState.OPTION) Cursor.GetComponent<RectTransform>().transform.position = cPositionOption[cursorPosOption].position;
     }
+
+
+    // Scene ---------------------------------------------------------------------
+    #region Change Scene /
 
     public void toTitle()
     {
         state = MenuState.TITLE;
-        changeScreen(0);
+        changeScreen(0, false);
+        cursorPos = 0;
     }
 
     public void toMenu()
     {
         state = MenuState.MENU;
-        changeScreen(1);
+        changeScreen(1, true);
+        cursorPosOption = 0;
     }
 
     public void toCharSelect()
     {
         state = MenuState.CHARSELECT;
-        changeScreen(2);
+        changeScreen(2, false);
         CharacterSelection.Instance.asignPlayers(players);
     }
 
+    public void toOption()
+    {
+        state = MenuState.OPTION;
+        changeScreen(3, true);
+    }
 
-    public void changeScreen(int iref)
+
+    public void changeScreen(int iref, bool cursorOn)
     {
         for (int i = 0; i < menuScreens.Count; i++)
         {
             if (i == iref) menuScreens[i].SetActive(true);
             else menuScreens[i].SetActive(false);
+
             
         }
+        if (cursorOn) Cursor.SetActive(true);
+        else Cursor.SetActive(false);
     }
+
+    #endregion
+
+    // Volume ---------------------------------------------------------------------
+    #region VOLUME 
+    public void SetVolumeGen()
+    {
+
+        AkSoundEngine.SetRTPCValue("User_RTPC_Main_Volume", Sliders[0].value);
+        PlayerPrefs.SetFloat("MainVolume", Sliders[0].value);
+    }
+
+    public void SetVolumeMusic()
+    {
+     
+        AkSoundEngine.SetRTPCValue("User_RTPC_Music_Volume", Sliders[1].value);
+        PlayerPrefs.SetFloat("MusicVolume", Sliders[1].value);
+    }
+
+    public void SetVolumeSFX()
+    {
+
+        AkSoundEngine.SetRTPCValue("User_RTPC_SFX_Volume", Sliders[2].value);
+        PlayerPrefs.SetFloat("SFXVolume", Sliders[2].value);
+    }
+
+    void loadMusicVolume()
+    {
+        Sliders[0].value = PlayerPrefs.GetFloat("MainVolume");
+        Sliders[1].value = PlayerPrefs.GetFloat("MusicVolume");
+        Sliders[2].value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetVolumeGen();
+        SetVolumeMusic();
+        SetVolumeSFX();
+    }
+    #endregion
 }
