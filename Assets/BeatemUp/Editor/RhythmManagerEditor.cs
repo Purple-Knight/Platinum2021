56005ef054d53fda9bb4a6dd259e8d0a9a06d478
Franci <algera.franci@gmail.com>
2021-12-10 16:12:24 +0100

    small fixes

--- Assets/BeatemUp/Editor/RhythmManagerEditor.cs
+++ Assets/BeatemUp/Editor/RhythmManagerEditor.cs
@@ -21,13 +21,8 @@ public class RhythmManagerEditor : Editor
         SerializedProperty hardPercentage = serializedObject.FindProperty("hardPercentage");
         SerializedProperty BPM = serializedObject.FindProperty("BPM");
 
-        /*EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("Editor Viual Offset : ");
-        inspectorOffset = EditorGUILayout.FloatField(inspectorOffset);
-        EditorGUILayout.EndHorizontal();
-*/
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("BPM : ");
+        EditorGUILayout.LabelField("BPM : "); // visualise in seconds the time lapse with BPM
         BPM.floatValue = EditorGUILayout.FloatField(BPM.floatValue);
         EditorGUILayout.EndHorizontal();
 
@@ -44,21 +39,23 @@ public class RhythmManagerEditor : Editor
         EditorGUI.LabelField(new Rect((Screen.width - 125) , inspectorOffset, 100, 10),"Half beat");
         
 
-        // Visuals
+        // Visuals color rectangles, half beat
         EditorGUI.DrawRect(new Rect(20 , 30 + inspectorOffset, Screen.width - 95, 30), Color.gray);
         EditorGUI.DrawRect(new Rect(20 , 30 + inspectorOffset, (Screen.width - 95) * easyPercentage.floatValue /100 , 30), Color.green);
         EditorGUI.DrawRect(new Rect(20 , 30 + inspectorOffset, (Screen.width - 95) * mediumPercentage.floatValue/100, 30), Color.yellow);
         EditorGUI.DrawRect(new Rect(20 , 30 + inspectorOffset, (Screen.width - 95) * hardPercentage.floatValue/100, 30), Color.red);
 
+        //seconds of each difficulties 
         EditorGUI.LabelField(new Rect((Screen.width - 95) * hardPercentage.floatValue/100, 70 + inspectorOffset, 100, 10), (beatDuration * hardPercentage.floatValue /100 ).ToString() + "s");
         EditorGUI.LabelField(new Rect((Screen.width - 95) * mediumPercentage.floatValue / 100, 70+ inspectorOffset, 100, 10), (beatDuration * mediumPercentage.floatValue / 100).ToString() + "s");
         EditorGUI.LabelField(new Rect((Screen.width - 95) * easyPercentage.floatValue / 100, 70 + inspectorOffset, 100, 15), (beatDuration * easyPercentage.floatValue / 100).ToString() + "s");
         EditorGUI.LabelField(new Rect((Screen.width - 100) , 70 + inspectorOffset, 100, 10),beatDuration.ToString() +"s");
 
         EditorGUILayout.Space( 80 );
-        EditorGUILayout.LabelField("Set you values here");
+        EditorGUILayout.LabelField("Set your values here");
         EditorGUILayout.LabelField("Easy Percentage :");
 
+        //Sliders for each level
         easyPercentage.floatValue = EditorGUI.Slider(new Rect(20, 120+ inspectorOffset, (Screen.width - 40), 20) , easyPercentage.floatValue, 1, 100);
         easyPercentage.floatValue = Mathf.Clamp(easyPercentage.floatValue, mediumPercentage.floatValue, 100);
 
@@ -76,6 +73,7 @@ public class RhythmManagerEditor : Editor
 
         EditorGUILayout.Space(60);
 
+        //visual for an entire beat time
         EditorGUI.DrawRect(new Rect(20, 240 + inspectorOffset, Screen.width - 40, 20), Color.gray);
         EditorGUI.DrawRect(new Rect(20 + (Screen.width - 40) * (((100 - easyPercentage.floatValue) /2)/ 100), 240 + inspectorOffset, (Screen.width - 40) * easyPercentage.floatValue / 100, 20), Color.green);
         EditorGUI.DrawRect(new Rect(20 + (Screen.width - 40) * (((100 - mediumPercentage.floatValue) / 2) / 100), 240 + inspectorOffset, (Screen.width - 40) * mediumPercentage.floatValue / 100, 20), Color.yellow);
